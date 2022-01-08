1644424002377fb06ddc814da6064c1737a25b1d
Ashley Sligh <asligh1@gmail.com>
2022-01-07 19:08:10 -0500

    modified dedupe

--- table_schemas.sql
+++ table_schemas.sql
@@ -17,27 +17,25 @@
 							"geometry" VARCHAR(200)
 						);	 
 						
-SELECT "Name" FROM bronze_billionaire; 	
-						
  CREATE TABLE silver_billionaire (
-							DISPLAY_NAME VARCHAR(50) PRIMARY KEY,
-	 						FIRST_NAME VARCHAR(20),
-	 					    MIDDLE_NAME VARCHAR(20),
-	 						LAST_NAME VARCHAR(20),
-	 						SUFFIX VARCHAR(10),
-	 						NET_WORTH DECIMAL,
-	 						COUNTRY VARCHAR(20),
-	 						RESIDENCE VARCHAR(30),
-	 	 					CITIZENSHIP VARCHAR(30),
-	 						SOURCE_OF_WEALTH VARCHAR(30),
-	 					    WEALTH_RANK INTEGER
-	 						AGE INTEGER
-							RELATIONSHIP_STATUS VARCHAR(30),
-	 						CHILDREN INTEGER,
-	 				        EDUCATION VARCHAR(100),
-	 						IS_SELF_MADE BOOLEAN,
-	 					    GEOMETRY POINT,
-	 					    LONGITUDE VARCHAR(200),
-						    LATITUDE VARCHAR(200)
+	 						ID SERIAL PRIMARY KEY,
+							"Display_Name" VARCHAR(50),
+	 						"First_Name" VARCHAR(20),
+	 					    "Middle_Name" VARCHAR(20),
+	 						"Last_Name" VARCHAR(20),
+	 						"Suffix" VARCHAR(10),
+	 						"Net_Worth" DECIMAL,
+	 						"Country" VARCHAR(100),
+	 						"Residence" VARCHAR(50),
+	 	 					"Citizenship" VARCHAR(50),
+	 						"Source_Of_Wealth" VARCHAR(100),
+	 					    "Wealth_Rank" INTEGER,
+	 						"Age" INTEGER,
+							"Relationship_Status" VARCHAR(50),
+	 						"Children" INTEGER,
+	 				        "Education" VARCHAR(400),
+	 						"Is_Self_Made" BOOLEAN,
+	 					    "Geometry" VARCHAR(200),
+	 					    "Longitude" VARCHAR(200),
+						    "Latitude" VARCHAR(200)
 						);	 
- 
