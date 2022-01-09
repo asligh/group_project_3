3783e7e34c44dd6b3a39959380c9621f2387bd96
Ashley Sligh <asligh1@gmail.com>
2022-01-08 21:20:03 -0500

    added articles and metrics

---
+++ config.py
@@ -0,0 +1,6 @@
+article_days = '30'
+forbes_billionaire_csv_path = "data/forbes_billionaires_geo.csv"
+silver_billionaire_csv_path = "data/silver_billionaires.csv"
+news_article_csv_path       = "data/news_articles.csv"
+news_metrics_csv_path       = "data/news_metrics.csv"
+news_api_url_top_headlines  = "https://newsapi.org/v2/everything?"
--- table_schemas.sql
+++ table_schemas.sql
@@ -1,4 +1,4 @@
- DROP TABLE IF EXISTS news_metrics CASCADE;
+ DROP TABLE IF EXISTS news_metric CASCADE;
  DROP TABLE IF EXISTS news_article CASCADE;
  DROP TABLE IF EXISTS bronze_billionaire CASCADE;
  DROP TABLE IF EXISTS silver_billionaire CASCADE;
@@ -22,7 +22,7 @@
 	
  --The cleaned CSV data to be used as the source of truth for the project	
  CREATE TABLE silver_billionaire (
-	 						id SERIAL PRIMARY KEY,
+	 						billionaire_id INTEGER PRIMARY KEY,
 							display_name VARCHAR(50) NOT NULL,
 	 						first_name VARCHAR(20),
 	 					    middle_name VARCHAR(20),
@@ -45,22 +45,23 @@
 						);	 
 						
 CREATE TABLE news_article (
-
-				id SERIAL PRIMARY KEY,
-				billionaire_id integer NOT NULL,
-				article_url VARCHAR(50) NOT NULL,
-				article_source VARCHAR(50) NOT NULL,
-				FOREIGN KEY (billionaire_id) REFERENCES silver_billionaire(id)
+				billionaire_id INTEGER,
+				publication VARCHAR(500),
+				author VARCHAR(500),
+				title VARCHAR(500),
+				url  VARCHAR(500),
+				published_ts TIMESTAMP,
+				popularity_rank INTEGER,
+				FOREIGN KEY (billionaire_id) REFERENCES silver_billionaire(billionaire_id)
 			);
 			
-CREATE TABLE news_metrics (
-				id SERIAL PRIMARY KEY,
+CREATE TABLE news_metric (
 				billionaire_id integer NOT NULL,
-				article_total_1_month integer NOT NULL,
-				api_source VARCHAR(10),
-				FOREIGN KEY (billionaire_id) REFERENCES silver_billionaire(id)
+				total_article_count integer NOT NULL,
+				FOREIGN KEY (billionaire_id) REFERENCES silver_billionaire(billionaire_id)
 			);
 
-select * from silver_billionaire where middle_name is not null and last_name = 'Marinho' order by display_name;
+select * from silver_billionaire;
 select * from news_article;
-select * from news_metrics;
+select * from news_metric;
+select 'success' as status;
