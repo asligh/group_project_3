ddb85890aa5f60c33d75baf231d97b9f6d7c2fcb
Ashley Sligh <asligh1@gmail.com>
2022-01-16 18:13:31 -0500

    Add files via upload
    Updated dropdown to bind both ID and Name from a list of dictionaries

--- static/js/bill2.js
+++ static/js/bill2.js
@@ -1,4 +1,5 @@
-// js file for the info.html page
+//js file for the info.html page
+
 async function initializePage(country)
 {
   let indBillionaires = [];
@@ -13,33 +14,50 @@ async function initializePage(country)
     const info_response = await fetch(info_url);
     const data = await info_response.json();
   
-    for (let i = 0; i < data[0].length; i++) 
+    for (let i = 0; i < data[0].length; i++)
     {
-      if (selected_country == data[0][i].county) 
+      if (selected_country == data[0][i].county)
       {
-        let name = data[0][i].display_name;
-        indBillionaires.push(name);
+        let billionaire_id   = data[0][i].billionaire_id;
+        let billionaire_name = data[0][i].display_name;
+        let billionaire_map  = {"billionaire_id" : billionaire_id, "billionaire_name" : billionaire_name};
+
+        indBillionaires.push(billionaire_map);
       }
     }
-  
+    
+    //https://www.codegrepper.com/code-examples/javascript/sort+an+array+of+dictionaries+elements+javascript
+    //https://stackoverflow.com/questions/10198257/comparing-2-strings-alphabetically-for-sorting-purposes
+    indBillionaires.sort(function(a, b)
+    {
+      return a.billionaire_name.localeCompare(b.billionaire_name);
+    });
+
     indBillionaires.sort();
-  
-    // appending the list to the dropdown
+    const section = document.querySelector('#selDataset');
+
     for (let x = 0; x < indBillionaires.length; x++) 
     {
-      text = indBillionaires[x];
-      const section = document.querySelector('#selDataset');
-      option = document.createElement("option");
-      option.setAttribute("value", text);
-      option.textContent = `${text}`;
-      section.append(option);
+       let text  = indBillionaires[x]["billionaire_name"];
+       let value = indBillionaires[x]["billionaire_id"];
+
+       option = document.createElement("option");
+       option.setAttribute("value", value);
+       option.textContent = `${text}`;
+       section.append(option);
     }
   }
 
   if(indBillionaires.length == 0)
   {
-    loadBillionairesByCountry(country);
+     loadBillionairesByCountry(country);
   }
+};
+
+function optionChanged(billionaire_id)
+{
+  alert('billionaire id is ' + billionaire_id)
+  loadNewsArticles(billionaire_id)
 };
 
 initializePage();
