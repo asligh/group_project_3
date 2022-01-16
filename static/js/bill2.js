c27001520171554bac4ef5c7dd31555eec73b43c
Ashley Sligh <asligh1@gmail.com>
2022-01-16 17:01:36 -0500

    Add files via upload
    Added code to pull the id from the URL

--- static/js/bill2.js
+++ static/js/bill2.js
@@ -1,36 +1,45 @@
 // js file for the info.html page
+async function initializePage(country)
+{
+  let indBillionaires = [];
 
-async function optionChanged(country) {
-
-  //alert("Youre in billionaire js");
-  let info_url = "/billionaire";
-  const info_response = await fetch(info_url);
-  const data = await info_response.json();
-
-  for (let i = 0; i < data[0].length; i++) {
-
-    if (country == data[0][i].county) {
-
-      // creating list of billionaires based on country
-      indBillionaires = []
-
-      name = data[0][i].display_name;
-      indBillionaires.push(name)
-      console.log(indBillionaires)
-
-      // appending the list to the dropdown
-
-      for (let x = 0; x < indBillionaires.length; x++) {
-        text = indBillionaires[x]
-        const section = document.querySelector('#selDataset')
-        option = document.createElement("option")
-        option.setAttribute("value", text)
-        option.textContent = `${text}`
-        section.append(option)
+  async function loadBillionairesByCountry()
+  {
+    const queryString  = window.location.search;
+    const urlParams    = new URLSearchParams(queryString);
+    const selected_country = urlParams.get('id');
+  
+    let info_url = "/billionaire";
+    const info_response = await fetch(info_url);
+    const data = await info_response.json();
+  
+    for (let i = 0; i < data[0].length; i++) 
+    {
+      if (selected_country == data[0][i].county) 
+      {
+        let name = data[0][i].display_name;
+        indBillionaires.push(name);
       }
     }
+  
+    indBillionaires.sort();
+  
+    // appending the list to the dropdown
+    for (let x = 0; x < indBillionaires.length; x++) 
+    {
+      text = indBillionaires[x];
+      const section = document.querySelector('#selDataset');
+      option = document.createElement("option");
+      option.setAttribute("value", text);
+      option.textContent = `${text}`;
+      section.append(option);
+    }
+  }
 
+  if(indBillionaires.length == 0)
+  {
+    loadBillionairesByCountry(country);
   }
 };
 
-optionChanged();
+initializePage();
